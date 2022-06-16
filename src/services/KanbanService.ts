5b93ba5f035ec4ce0abd18d4314d8f74a23a4405
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-06-15 22:22:36 -0600

    Use "production" server

--- src/services/KanbanService.ts
+++ src/services/KanbanService.ts
@@ -1,15 +1,24 @@
+const production = {
+  url: 'https://my-simple-kanban.herokuapp.com'
+};
+const development = {
+  url: 'http://localhost:4000'
+};
+const backend = process.env.NODE_ENV === 'development' ? development : production;
+
 export async function getAllBoards() {
   try {
-    const response = await fetch('/api/v1/boards');
+    const response = await fetch(`${backend.url}/api/v1/boards`);
     return await response.json();
   } catch (error) {
+    console.log(error);
     return [];
   }
 }
 
 export async function getBoard(id: string | undefined) {
   try {
-    const response = await fetch(`/api/v1/boards/${id}`);
+    const response = await fetch(`${backend.url}/api/v1/boards/${id}`);
     return await response.json();
   } catch (error) {
     return [];
@@ -18,7 +27,7 @@ export async function getBoard(id: string | undefined) {
 
 export async function createBoard(title: string | undefined, description: string | undefined) {
   try {
-    const response = await fetch('/api/v1/boards/', {
+    const response = await fetch(`${backend.url}/api/v1/boards/`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json, text/plain, */*',
@@ -35,7 +44,7 @@ export async function createBoard(title: string | undefined, description: string
 
 export async function updateBoardTitle(board: string | undefined, title: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}`;
+    const url = `${backend.url}/api/v1/boards/${board}`;
     const response = await fetch(url, {
       method: 'PATCH',
       body: JSON.stringify({title: title}),
@@ -50,7 +59,7 @@ export async function updateBoardTitle(board: string | undefined, title: string 
 
 export async function updateBoardDescription(board: string | undefined, description: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}`;
+    const url = `${backend.url}/api/v1/boards/${board}`;
     const response = await fetch(url, {
       method: 'PATCH',
       body: JSON.stringify({description: description}),
@@ -66,7 +75,7 @@ export async function updateBoardDescription(board: string | undefined, descript
 
 export async function createColumn(board: string | undefined, title: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}/columns/`;
+    const url = `${backend.url}/api/v1/boards/${board}/columns/`;
     const response = await fetch(url, {
       method: 'POST',
       headers: {
@@ -84,7 +93,7 @@ export async function createColumn(board: string | undefined, title: string | un
 
 export async function createTask(board: string | undefined, column: string | undefined, description: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}/columns/${column}/tasks`;
+    const url = `${backend.url}/api/v1/boards/${board}/columns/${column}/tasks`;
     const response = await fetch(url, {
       method: 'POST',
       headers: {
@@ -102,7 +111,7 @@ export async function createTask(board: string | undefined, column: string | und
 
 export async function updateTask(board: string | undefined, column: string | undefined, task: string | undefined, description: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}/columns/${column}/tasks/${task}`;
+    const url = `${backend.url}/api/v1/boards/${board}/columns/${column}/tasks/${task}`;
     const response = await fetch(url, {
       method: 'PATCH',
       body: JSON.stringify({description: description}),
@@ -117,7 +126,7 @@ export async function updateTask(board: string | undefined, column: string | und
 
 export async function deleteTask(board: string | undefined, column: string | undefined, task: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}/columns/${column}/tasks/${task}`;
+    const url = `${backend.url}/api/v1/boards/${board}/columns/${column}/tasks/${task}`;
     const response = await fetch(url, {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' }
@@ -131,7 +140,7 @@ export async function deleteTask(board: string | undefined, column: string | und
 
 export async function updateColumn(board: string | undefined, column: string | undefined, title: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}/columns/${column}`;
+    const url = `${backend.url}/api/v1/boards/${board}/columns/${column}`;
     const response = await fetch(url, {
       method: 'PATCH',
       body: JSON.stringify({title: title}),
@@ -146,7 +155,7 @@ export async function updateColumn(board: string | undefined, column: string | u
 
 export async function deleteColumn(board: string | undefined, column: string | undefined) {
   try {
-    const url = `/api/v1/boards/${board}/columns/${column}`;
+    const url = `${backend.url}/api/v1/boards/${board}/columns/${column}`;
     const response = await fetch(url, {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' }
@@ -161,7 +170,7 @@ export async function deleteColumn(board: string | undefined, column: string | u
 
 export async function moveTask(board: number, task: number, destinationColumn: number, destinationPosition: number) {
   try { 
-    const url = `/api/v1/boards/${board}/move-task`;
+    const url = `${backend.url}/api/v1/boards/${board}/move-task`;
     const response = await fetch(url, {
       method: 'POST',
       headers: {
@@ -179,7 +188,7 @@ export async function moveTask(board: number, task: number, destinationColumn: n
 
 export async function moveColumn(board: number, column: number, destinationPosition: number) {
   try { 
-    const url = `/api/v1/boards/${board}/move-column`;
+    const url = `${backend.url}/api/v1/boards/${board}/move-column`;
     const response = await fetch(url, {
       method: 'POST',
       headers: {
