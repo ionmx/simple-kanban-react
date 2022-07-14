7b8fd9dd1f07758160bd0eff2595e77b6ca4d17f
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-14 00:07:22 -0600

    Revert "Create eslint.yml"
    This reverts commit c83725679c999b76c855e01107cfc279cfe6e4d6.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'standard'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
     'react',
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
   rules: {
-  },
-};
+  }
+}
